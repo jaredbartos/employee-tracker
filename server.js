952caa3d009ed437d6774c32689b0e59b166536d
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-11-16 14:37:00 -0600

    Add comments

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-const init = require('./lib/prompt');
+// Import required modules
+const figlet = require('figlet');
+const taskPrompt = require('./lib/prompt');
 
+// Function to initialize app
+const init = () => {
+  // Create title heading
+  console.log('\n\n------------------------------------------------------------------------------------------\n\n');
+  console.log(figlet.textSync('Employee Tracker'));
+  console.log('\n\n------------------------------------------------------------------------------------------\n\n');
+  // Initialize inquirer prompts
+  taskPrompt();
+};
+
+// Initialize app
 init();
